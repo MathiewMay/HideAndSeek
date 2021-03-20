adf4563afd54615b722761fdbe8fa1320d9888e7
BuildTools <unconfigured@null.spigotmc.org>
2021-03-19 21:58:56 -0400

    0.2.1
    Fixed
    - Set the startCountdown time from 5 to 3 (forgot to set it back before pushing)

--- src/main/java/ca/mathiewmay/hideandseek/GameSettings.java
+++ src/main/java/ca/mathiewmay/hideandseek/GameSettings.java
@@ -3,7 +3,7 @@ package ca.mathiewmay.hideandseek;
 public class GameSettings {
     public static final int minPlayers = 2;
 
-    public static final int startCountdown = 5;
+    public static final int startCountdown = 30;
     public static final int gameCountdown = 300;
     public static final int hCountdown = 20;
 }
